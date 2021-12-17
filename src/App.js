8bed1da6953a5231b66928535e234695b61cfa14
monish karunakaran <ckmonish2000@gmail.com>
2021-12-17 15:42:47 +0530

    get state value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { ethers } from "ethers"
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
 
 function App() {
+  const [value, setvalue] = useState("")
+  const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
   console.log(Greeter.abi)
+
+  // request access to meta mask accounts
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" })
+  }
+
+
+  async function get_greeting() {
+    if (typeof (window.ethereum) !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(contract_address, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
   return (
     <div>
-      hello
+      <input placeholder="greeting" onChange={(e => setvalue(e.target.value))} />
+      <button onClick={requestAccount}>Login</button>
+      <button onClick={get_greeting}>Get</button>
     </div>
   );
 }
