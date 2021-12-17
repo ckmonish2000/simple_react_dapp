8fb7c82f9553ee8e92e3a0f3eebad5c8872ae6d8
monish karunakaran <ckmonish2000@gmail.com>
2021-12-17 16:07:22 +0530

    working dapp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
 
 function App() {
   const [value, setvalue] = useState("")
+  const [value2, setvalue2] = useState("")
   const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
   console.log(Greeter.abi)
@@ -20,18 +21,36 @@ function App() {
       const contract = new ethers.Contract(contract_address, Greeter.abi, provider)
       try {
         const data = await contract.greet()
-        console.log(data)
+        setvalue2(data)
       } catch (err) {
         console.log(err)
       }
     }
   }
 
+  async function set_greeting() {
+    if (typeof (window.ethereum) !== "undefined") {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log("calling signer")
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(contract_address, Greeter.abi, signer)
+
+      const transaction = await contract.setGreeting(value)
+      console.log(transaction)
+      await transaction.wait()
+      get_greeting()
+
+
+    }
+  }
+
   return (
     <div>
+      <h1>{value2}</h1>
       <input placeholder="greeting" onChange={(e => setvalue(e.target.value))} />
-      <button onClick={requestAccount}>Login</button>
-      <button onClick={get_greeting}>Get</button>
+      <button onClick={set_greeting}>set</button>
+      {/* <button onClick={get_greeting}>Get</button> */}
     </div>
   );
 }
